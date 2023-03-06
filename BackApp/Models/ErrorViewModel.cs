f4f5f6b1a513403842f8ac5653e6aa2d34947d59
sellzx <simlara89@gmail.com>
2023-03-06 19:04:28 -0300

    First commit

---
+++ BackApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BackApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
